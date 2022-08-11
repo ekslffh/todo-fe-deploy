1ac84940d31542020046132ab5d4057bcec9afc5
ekslffh <ekslffh@gmail.com>
2022-08-11 19:37:42 +0900

    change backend url + https

--- src/app-config.js
+++ src/app-config.js
@@ -4,10 +4,11 @@ const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
     // backendHost = "http://localhost:8080";
-    backendHost = "http://production-todo-be.us-west-2.elasticbeanstalk.com";
+    backendHost = "https://api.nugul.link";
 }
 else {
-    backendHost = "http://production-todo-be.us-west-2.elasticbeanstalk.com";
+    // backendHost = "http://production-todo-be.us-west-2.elasticbeanstalk.com";
+    backendHost = "https://api.nugul.link";
 }
 
 export const API_BASE_URL = `${backendHost}`;
