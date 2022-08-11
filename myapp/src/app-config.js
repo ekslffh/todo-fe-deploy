5f02de5163505087b5ca67432aaa08cc30635ff6
ekslffh <ekslffh@gmail.com>
2022-08-11 22:55:00 +0900

    before ssl

---
+++ myapp/src/app-config.js
@@ -0,0 +1,14 @@
+let backendHost;
+
+const hostname = window && window.location && window.location.hostname;
+
+if (hostname === "localhost") {
+    // backendHost = "http://localhost:8080";
+    backendHost = "https://api.nugul.link";
+}
+else {
+    // backendHost = "http://production-todo-be.us-west-2.elasticbeanstalk.com";
+    backendHost = "https://api.nugul.link";
+}
+
+export const API_BASE_URL = `${backendHost}`;
--- src/app-config.js
+++ src/app-config.js
@@ -4,11 +4,12 @@ const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
     // backendHost = "http://localhost:8080";
-    backendHost = "https://api.nugul.link";
+    // backendHost = "http://api.nugul.link";
+    backendHost = "http://production-todo-be.us-west-2.elasticbeanstalk.com";
 }
 else {
-    // backendHost = "http://production-todo-be.us-west-2.elasticbeanstalk.com";
-    backendHost = "https://api.nugul.link";
+    backendHost = "http://production-todo-be.us-west-2.elasticbeanstalk.com";
+    // backendHost = "http://api.nugul.link";
 }
 
 export const API_BASE_URL = `${backendHost}`;
